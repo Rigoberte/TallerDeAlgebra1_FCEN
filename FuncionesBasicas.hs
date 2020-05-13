a2a7a9d7991bdff29a9a1e4b15e5d427dbc609c4
Rigoberte <65089651+Rigoberte@users.noreply.github.com>
2020-05-13 10:54:03 -0300

    Update FuncionesBasicas.hs

--- FuncionesBasicas.hs
+++ FuncionesBasicas.hs
@@ -37,3 +37,24 @@ sumatoria n|n==1 = 1
 factorial :: Int -> Int
 factorial n|n == 0 = 1
  |otherwise = n * factorial (n-1)
+
+digitosiguales:: Integer -> Bool
+digitosiguales n |n < 10 = True
+ |otherwise = ( (mod n 10) == (mod (div n 10) 10) ) && digitosiguales (div n 10)
+
+cantDigitos:: Integer -> Integer
+cantDigitos n |(n < 10) = 1
+ |otherwise = 1 + cantDigitos (div n 10)
+
+esDivisor:: Integer -> Integer -> Bool
+esDivisor n m = mod n m == 0
+
+esPotencia:: Integer -> Integer -> Bool
+esPotencia n m =  n == 1 || n >= m &&  esPotencia (div n m) m && mod n m == 0
+
+tieneDivisoresDesde :: Int -> Int -> Bool
+tieneDivisoresDesde n k |k == n = False
+ |otherwise = (mod n k == 0) || tieneDivisoresDesde n ( k + 1 )
+
+esPrimo:: Int -> Bool
+esPrimo n = (n > 1) && not (tieneDivisoresDesde n 2)
