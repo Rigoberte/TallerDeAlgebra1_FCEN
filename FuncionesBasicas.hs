c6f8539bd763dd47dbaaafed7850c5a5e74be2e9
Rigoberte <65089651+Rigoberte@users.noreply.github.com>
2020-07-09 15:07:21 -0300

    Update FuncionesBasicas.hs

--- FuncionesBasicas.hs
+++ FuncionesBasicas.hs
@@ -1,40 +1,40 @@
 module FuncionesBasicas
 where
 
-suma :: Int -> Int -> Int
+suma:: Int -> Int -> Int
 suma x y = x + y
 
-esPar :: Int -> Bool
+esPar:: Int -> Bool
 esPar x = mod x 2 == 0
 
-esImpar :: Int -> Bool
+esImpar:: Int -> Bool
 esImpar x = not(esPar x)
 
-normaVectorial :: (Float, Float) -> Float
+normaVectorial:: (Float, Float) -> Float
 normaVectorial (x, y) = sqrt(x^2 + y^2)
 
-signo :: Float -> Int
+signo:: Float -> Int
 signo x | x == 0 = 0
  |(x > 0) = 1
  |otherwise = (-1)
 
-maximo :: Float -> Float -> Float
+maximo:: Float -> Float -> Float
 maximo x y |x > y = x
  |otherwise = (y)
 
-minimo :: Float -> Float -> Float
+minimo:: Float -> Float -> Float
 minimo x y |x > y = y
  |otherwise = (x)
 
-restaNatural :: Int -> Int -> Int
+restaNatural:: Int -> Int -> Int
 restaNatural n m|((m > n) || (m == n)) = 0
  |otherwise = n - m
 
-sumatoria :: Int -> Int
+sumatoria:: Int -> Int
 sumatoria n|n==1 = 1
  |otherwise = n + sumatoria(n-1)
 
-factorial :: Int -> Int
+factorial:: Integer -> Integer
 factorial n|n == 0 = 1
  |otherwise = n * factorial (n-1)
 
@@ -58,3 +58,35 @@ tieneDivisoresDesde n k |k == n = False
 
 esPrimo:: Int -> Bool
 esPrimo n = (n > 1) && not (tieneDivisoresDesde n 2)
+
+--Listas======================================
+--L===========================================
+
+len:: Set Int -> Int
+len list |tail list == [] = 1
+ |otherwise = 1 + len (tail list)
+
+pertenece::[Int] -> Int -> Bool
+pertenece list m|list==[] = False
+ |head list == m = True
+ |otherwise = pertenece (tail list) m
+
+union::Set Int -> Set Int -> Set Int
+union [] c2 = c2
+union c1 c2 |not(pertenece c2 (head c1)) = union (tail c1) (head c1:c2)
+ |otherwise = union (tail c1) c2
+
+interseccion::Set Int -> Set Int -> Set Int
+interseccion c1 c2 = intersec c1 c2 []
+
+intersec::Set Int -> Set Int -> Set Int -> Set Int
+intersec [] c2 c3 = c3
+intersec c1 c2 c3 |pertenece c2 (head c1) = intersec (tail c1) c2 (head c1:c3)
+ |otherwise = intersec (tail c1) c2 c3
+
+voltereta:: Set Int -> Set Int
+voltereta list = vuelta list []
+
+vuelta::Set Int -> Set Int -> Set Int
+vuelta list res |tail list == [] = head list:res
+ |otherwise = vuelta (tail list) (head list:res)
